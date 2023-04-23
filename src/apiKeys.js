693781697adcb4dc29c3b9918c548656be395b15
Gaurav1756 <gauravsenger2167@gmail.com>
2023-04-23 23:33:06 +0530

    weather

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "4a1d36184b2322559b8523ef621953c3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
